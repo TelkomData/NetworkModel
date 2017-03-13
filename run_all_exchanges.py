9ca526ceb5334b70c59444cdd02c06f63060b2da
AidanHelmboldTBD <helmboa@telkom.co.za>
2017-03-13 13:29:41 +0200

    Update

--- run_all_exchanges.py
+++ run_all_exchanges.py
@@ -18,10 +18,7 @@ def main(options):
             tf.gfile.DeleteRecursively(save_files_to)
         tf.gfile.MakeDirs(save_files_to)
 
-<<<<<<< HEAD
-=======
-        # dewald 
->>>>>>> refs/remotes/origin/AidanHelmboldTBD-patch-1
+
         X_train, X_test, y_train, y_test = load_exchange(FLAGS.data_dir + exc, x_sum=False, y_loc=True)
 
         models = {}
